c6efeb7f36a195a76e310603410eeb64adc28782
Asaleem1212 <145053931+Asaleem1212@users.noreply.github.com>
2024-01-17 17:43:55 +0000

    Update Importing Data queries.sql

--- Project 2/Importing Data queries.sql
+++ Project 2/Importing Data queries.sql
@@ -2,23 +2,23 @@
 
 -- Import categories.csv into categories table
 COPY categories(category_id, category)
-FROM 'D:\Data Scie - Fiverr Gigs\Starter_Files\Resources\category.csv' 
+FROM ' "C:\Users\Ahmed\OneDrive\Documents\Project 2\Project 2\Resources\category.csv"
 DELIMITER ',' 
 CSV HEADER;
 
 -- Import subcategories.csv into subcategories table
 COPY subcategories(subcategory_id, subcategory)
-FROM 'D:\Data Scie - Fiverr Gigs\Starter_Files\Resources\subcategory.csv'
+FROM "C:\Users\Ahmed\OneDrive\Documents\Project 2\Project 2\Resources\subcategory.csv"
 DELIMITER ',' 
 CSV HEADER;
 
 -- Import contacts.csv into contacts table
 COPY contacts(contact_id, first_name, last_name, email)
-FROM 'D:\Data Scie - Fiverr Gigs\Starter_Files\Resources\contacts.csv' 
+FROM "C:\Users\Ahmed\OneDrive\Documents\Project 2\Project 2\Resources\contacts.csv"
 WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 
 -- Import campaigns.csv into campaigns table
 COPY campaigns(cf_id, contact_id, company_name, description, goal, pledged, outcome, backers_count, country, currency, launch_date, end_date, category_id, subcategory_id)
-FROM 'D:\Data Scie - Fiverr Gigs\Starter_Files\Resources\campaign.csv' 
+FROM "C:\Users\Ahmed\OneDrive\Documents\Project 2\Project 2\Resources\campaign.csv" 
 WITH (FORMAT csv, HEADER true, DELIMITER ',');
